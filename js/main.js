8d19380007c0f28f5fc2d3b473d0916ace0a178a
mariondubuisson <55016984+mariondubuisson@users.noreply.github.com>
2020-03-29 17:20:14 +0200

    Modify the style with CSTB example

--- js/main.js
+++ js/main.js
@@ -6,9 +6,5 @@ const timelineEls = document.querySelectorAll(".timeline_container");
 
 timelineEls.forEach(timelineEl => {
   const timeline = new Timeline(timelineEl);
-
-  console.log("Mes timelines containers", timeline);
   timeline.initTimeline();
 });
-// regarde combien il y a d'item dans la section
-// retourne le nombre d'items
